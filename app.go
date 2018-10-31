4cc496b1f6084cc653de5dfb22f6e1ff2ff3341e
QTTQ <1321510155@qq.com>
2018-10-31 13:10:29 +0800

    用go自带的http启动gin

--- app.go
+++ app.go
@@ -6,6 +6,8 @@ import (
 	"paopao/config"
 	"paopao/models"
 	"paopao/routers"
+	"net/http"
+	"time"
 )
 func main() {
 	db.ConnectAndInit(
@@ -16,5 +18,13 @@ func main() {
 	)
 	defer db.DB.Close()
 	router := routers.InitRouters()
-	router.Run(":8080")
+	// router.Run(":8080") //用gin 路由启动 ----
+	s := &http.Server{
+        Addr:           ":8080",
+        Handler:        router,
+        ReadTimeout:    10 * time.Second,
+        WriteTimeout:   10 * time.Second,
+        MaxHeaderBytes: 1 << 20,
+    }
+    s.ListenAndServe() // listen and serve on 0.0.0.0:8080  //用http  启动 ----
 }
